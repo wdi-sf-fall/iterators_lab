7fba0cd704f48c10f6b49ac920de3323ebe2c2e6
Tim Licata <tim@timothylicata.com>
2014-10-08 09:57:01 -0700

    implementations and tests for iterators

--- iterators.js
+++ iterators.js
@@ -7,6 +7,53 @@ var iterators = {
         }
       }
       return max;
+    },
+    min: function (numList) {
+      var min = Infinity;
+      for (var i = 0; i < numList.length; i++) {
+        if (numList[i] < min) {
+          min = numList[i];
+        }
+      }
+      return min;
+    },
+    each: function (list, callback) {
+      for (var i = 0; i < list.length; i++) {
+        callback(list[i]);
+      }
+    },
+    map: function (list, callback) {
+      var output = [];
+      for (var i = 0; i < list.length; i++) {
+        output.push(callback(list[i]));
+      }
+      return output;
+    },
+    filter: function (list, callback) {
+      var output = [];
+      for (var i = 0; i < list.length; i++) {
+        if (callback(list[i])) {
+          output.push(list[i]);
+        }
+      }
+      return output;
+    },
+    reduce: function (list, callback, accumulator) {
+      if (typeof accumulator === "undefined") {
+        var copy = list.concat();
+        var first = copy.shift();
+        return iterators.reduce(copy, callback, first);
+      } else {
+        for (var i = 0; i < list.length; i++) {
+          accumulator = callback(list[i], accumulator);
+        }
+        return accumulator;
+      }
+    },
+    reject: function (list, callback) {
+      return iterators.filter(list, function (item) {
+        return !callback(item);
+      });
     }
   };
 
--- spec/iterators_test.js
+++ spec/iterators_test.js
@@ -3,8 +3,6 @@ var Iterators = require('../iterators');
 
 describe('Iterators', function () {
   describe('#max', function () {
-
-    // cleaner to declare the variable outside of the beforeEach function
     var myArr;
 
     // use beforeEach(function(){}); to create a variable before each test is run.
@@ -19,6 +17,84 @@ describe('Iterators', function () {
     it('should return -Infinity for empty array', function () {
       myArr = [];
       expect(Iterators.max(myArr)).to.equal(-Infinity);
+    });
+  });
+
+  describe('#min', function () {
+    it('should return the minimum in an array', function () {
+      expect(Iterators.min([1,2,3,4,5])).to.equal(1);
+      expect(Iterators.min([-1,0,3,4,5])).to.equal(-1);
+      expect(Iterators.min([0,3,4,5])).to.equal(0);
+    });
+    it('should return Infinity for an empty array', function () {
+      expect(Iterators.min([])).to.equal(Infinity);
+    });
+  });
+
+  describe('#each', function () {
+    it('should run a function for each element', function () {
+      var total = 0;
+      Iterators.each([1,2,3], function (num) {
+        total += num;
+      });
+      expect(total).to.equal(6);
+
+      var product = 1;
+      Iterators.each([2,3,4], function (num) {
+        product = product * num;
+      });
+      expect(product).to.equal(24);
+    });
+  });
+
+  describe('#map', function () {
+    it('should return an array of results', function () {
+      var timesTwo = function (num) {
+        return num * 2;
+      };
+      var plusThree = function (num) {
+        return num + 3;
+      };
+
+      expect(Iterators.map([1,2,3], timesTwo)).to.deep.equal([2,4,6]);
+      expect(Iterators.map([2,3,4], plusThree)).to.deep.equal([5,6,7]);
+    });
+  });
+
+  describe('#filter', function () {
+    it('should filter an array to items that pass test', function () {
+      var even = function (num) {
+        return num % 2 === 0;
+      }
+      var odd = function (num) {
+        return num % 2 !== 0;
+      };
+      expect(Iterators.filter([1,2,3,4,5,6], even)).to.deep.equal([2,4,6]);
+      expect(Iterators.filter([1,2,3,4,5,6], odd)).to.deep.equal([1,3,5]);
+    });
+  });
+
+  describe('#reduce', function () {
+    it('should accumulate over an array', function () {
+      var add = function (a, b) {
+        return a + b
+      };
+      expect(Iterators.reduce([1,2,3,4], add)).to.equal(10);
+      expect(Iterators.reduce([1,2,3,4], add, 0)).to.equal(10);
+      expect(Iterators.reduce([1,2,3,4], add, 1)).to.equal(11);
+    });
+  });
+
+  describe('#reject', function () {
+    it('should reject elements that fail test', function () {
+      var even = function (num) {
+        return num % 2 === 0;
+      }
+      var odd = function (num) {
+        return num % 2 !== 0;
+      };
+      expect(Iterators.reject([1,2,3,4], even)).to.deep.equal([1,3]);
+      expect(Iterators.reject([1,2,3,4], odd)).to.deep.equal([2,4]);
     });
   });
 });
